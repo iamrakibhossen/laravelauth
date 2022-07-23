b2e3c3992f355ac18ecb03dfbefa8348a731adfa
Rakib Hossen <58178311+iamrakibhossen@users.noreply.github.com>
2022-07-23 14:09:29 +0600

    Add files via upload

---
+++ storage/framework/views/b581cebb9cc3e4c37a50bef695eab053a02a9669.php
@@ -0,0 +1,31 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Homepage')); ?></div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if( Auth::user() ): ?>
+                        <?php echo e(__('You are logged in!')); ?>
+
+                    <?php else: ?>
+                        <?php echo e(__('You are logout!')); ?>
+
+                    <?php endif; ?>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH H:\larav\resources\views/welcome.blade.php ENDPATH**/ ?>
